83e9cea7eca6201eecae6f433510b80835a84a33
Sebastian-Medina185 <sebastianmedinaechavarria@gmail.com>
2026-04-05 15:38:56 -0500

    fix: update CORS for production

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const PORT = process.env.PORT || 3000;
 // ==================== MIDDLEWARES ====================
 // Permitir TODAS las peticiones en desarrollo
 app.use(cors({
-    origin: '*',
-    credentials: true
+    origin: 'https://stamp-lab-proyecto.vercel.app'
 }));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 const path = require('path');
